633e031155cc33b0ab431fe4e2c578ab33a24dfc
Raghav0079 <raghav051006@gmail.com>
2026-07-25 20:53:07 +0530

    commit 3

--- Main1.java
+++ Main1.java
@@ -1,16 +1,27 @@
+interface TaxCalculator {
+    double amountAfterTax(double amount);
+}
 
-class TaxCalculator{
-    public double amountAfterTax(double amount){
-        return(amount + 0.18 * amount);
+class IndianTax implements TaxCalculator {
+    public double amountAfterTax(double amount) {
+        return (amount + 0.18 * amount);
     }
 }
-class InvoiceService{
-    public void calculate(){
-        TaxCalculator taxCalculator = new TaxCalculator;
+
+class USTax implements TaxCalculator {
+    public double amountAfterTax(double amount) {
+        return (amount + 0.18 * amount);
+    }
+}
+
+class InvoiceService {
+    public void calculate() {
+        TaxCalculator taxCalculator = new IndianTax();
         taxCalculator.amountAfterTax(100);
     }
 }
-public class Main1{
+
+public class Main1 {
     public static void main(String[] args) {
         
     }
