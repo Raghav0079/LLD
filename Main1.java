e1172fe779bf4e26b66a85920420103edbc434c5
Raghav0079 <raghav051006@gmail.com>
2026-07-25 20:46:25 +0530

    commit 2

--- Main1.java
+++ Main1.java
@@ -4,7 +4,14 @@ class TaxCalculator{
         return(amount + 0.18 * amount);
     }
 }
-
+class InvoiceService{
+    public void calculate(){
+        TaxCalculator taxCalculator = new TaxCalculator;
+        taxCalculator.amountAfterTax(100);
+    }
+}
 public class Main1{
-    
+    public static void main(String[] args) {
+        
+    }
 }
