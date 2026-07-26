637dae744c63225d2aa2785be9a56afee7f674f0
Raghav0079 <raghav051006@gmail.com>
2026-07-26 21:03:03 +0530

    commit 10

--- Main2.java
+++ Main2.java
@@ -1,3 +1,16 @@
+class Notification{
+    public void sendNotification(){
+        System.out.println("email send");
+    }
+}
+
+
+
 public class Main2 {
+    public static void main(String[] args) {
+        Notification notification = new Notification();
+        notification.sendNotification();
+        
+    }
     
 }
