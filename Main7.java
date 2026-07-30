9b9d85508509664a6289f57cd1777fbd018ae57d
Raghav0079 <raghav051006@gmail.com>
2026-07-30 20:58:07 +0530

    commit 17

---
+++ Main7.java
@@ -0,0 +1,23 @@
+class JudgeAnalytics{
+    private static JudgeAnalytics judgeAnalytics = null;
+
+    private JudgeAnalytics(){
+
+    }
+    
+    public static JudgeAnalytics getInstance(){
+        if(judgeAnalytics == null){
+            judgeAnalytics = new JudgeAnalytics();
+        }
+        return judgeAnalytics;
+    }
+}
+
+public class Main7 {
+    public static void main(String[] args) {
+        JudgeAnalytics judgeAnalytics = JudgeAnalytics.getInstance();
+        JudgeAnalytics judgeAnalytics2 = JudgeAnalytics.getInstance();
+        System.out.println(judgeAnalytics);
+        System.out.println(judgeAnalytics2);
+    }
+}
