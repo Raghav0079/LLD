9f6c36fe5537dce84a1ff52b22854fe16f6aa07d
Raghav0079 <raghav051006@gmail.com>
2026-08-01 11:32:25 +0530

    commit 24

--- Main9.java
+++ Main9.java
@@ -1,24 +1,94 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
+// Represents a customizable Burger Meal
 class BurgerMeal {
-    private String bun;
-    private String patty;
-    private String sides;
-    private List<String> toppings;
+    // Required components
+    private final String bunType;
+    private final String patty;
 
+    // Optional components
+    private final boolean hasCheese;
+    private final List<String> toppings;
+    private final String side;
+    private final String drink;
 
-    public BurgerMeal(String bun, String patty, String sides, List<String> toppings) {
-        this.bun = bun;
-        this.patty = patty;
-        this.sides = sides;
-        this.toppings = toppings;
+    // Private constructor to force use of Builder
+    private BurgerMeal(BurgerBuilder builder) {
+        this.bunType = builder.bunType;
+        this.patty = builder.patty;
+        this.hasCheese = builder.hasCheese;
+        this.toppings = builder.toppings;
+        this.side = builder.side;
+        this.drink = builder.drink;
+    }
+
+    // Static nested Builder class
+    public static class BurgerBuilder {
+        // Required
+        private final String bunType;
+        private final String patty;
+
+        // Optional
+        private boolean hasCheese;
+        private List<String> toppings;
+        private String side;
+        private String drink;
+
+        // Builder constructor with required fields
+        public BurgerBuilder(String bunType, String patty) {
+            this.bunType = bunType;
+            this.patty = patty;
+        }
+
+        // Method to set cheese
+        public BurgerBuilder withCheese(boolean hasCheese) {
+            this.hasCheese = hasCheese;
+            return this;
+        }
+
+        // Method to set toppings
+        public BurgerBuilder withToppings(List<String> toppings) {
+            this.toppings = toppings;
+            return this;
+        }
+
+        // Method to set side
+        public BurgerBuilder withSide(String side) {
+            this.side = side;
+            return this;
+        }
+
+        // Method to set drink
+        public BurgerBuilder withDrink(String drink) {
+            this.drink = drink;
+            return this;
+        }
+
+        // Final build method
+        public BurgerMeal build() {
+            return new BurgerMeal(this);
+        }
     }
 }
 
-public class Main9 {
+class Main {
     public static void main(String[] args) {
-        
-        BurgerMeal burgerMeal = new BurgerMeal("Brioche", "Beef", "Fries", new ArrayList<>());
+        // Creating burger with only required fields
+        BurgerMeal plainBurger = new BurgerMeal.BurgerBuilder("wheat", "veg")
+                                    .build();
+
+        // Burger with cheese only
+        BurgerMeal burgerWithCheese = new BurgerMeal.BurgerBuilder("wheat", "veg")
+                                        .withCheese(true)
+                                        .build();
+
+        // Fully loaded burger
+        List<String> toppings = Arrays.asList("lettuce", "onion", "jalapeno");
+        BurgerMeal loadedBurger = new BurgerMeal.BurgerBuilder("multigrain", "chicken")
+                                        .withCheese(true)
+                                        .withToppings(toppings)
+                                        .withSide("fries")
+                                        .withDrink("coke")
+                                        .build();
     }
 }
