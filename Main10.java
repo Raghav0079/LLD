4179587d68319cff362a8f1a5242e189905809ef
Raghav0079 <raghav051006@gmail.com>
2026-08-02 19:13:44 +0530

    commit 27

--- Main10.java
+++ Main10.java
@@ -1,5 +1,4 @@
 
-
 // ========== Interfaces ==========
 interface PaymentGateway {
     void processPayment(double amount);
@@ -10,17 +9,9 @@ interface Invoice {
 }
 
 // ========== India Implementations ==========
-class RazorpayGateway implements PaymentGateway {
-    public void processPayment(double amount) {
-        System.out.println("Processing INR payment via Razorpay: " + amount);
-    }
-}
 
-class PayUGateway implements PaymentGateway {
-    public void processPayment(double amount) {
-        System.out.println("Processing INR payment via PayU: " + amount);
-    }
-}
+
+
 
 class GSTInvoice implements Invoice {
     public void generateInvoice() {
@@ -29,60 +20,11 @@ class GSTInvoice implements Invoice {
 }
 
 // ========== US Implementations ==========
-class PayPalGateway implements PaymentGateway {
-    public void processPayment(double amount) {
-        System.out.println("Processing USD payment via PayPal: " + amount);
-    }
-}
 
-class StripeGateway implements PaymentGateway {
-    public void processPayment(double amount) {
-        System.out.println("Processing USD payment via Stripe: " + amount);
-    }
-}
 
-class USInvoice implements Invoice {
-    public void generateInvoice() {
-        System.out.println("Generating Invoice as per US norms.");
-    }
-}
 
-// ========== Abstract Factory ==========
-interface RegionFactory {
-    PaymentGateway createPaymentGateway(String gatewayType);
-    Invoice createInvoice();
-}
 
-// ========== Concrete Factories ==========
-class IndiaFactory implements RegionFactory {
-    public PaymentGateway createPaymentGateway(String gatewayType) {
-        if (gatewayType.equalsIgnoreCase("razorpay")) {
-            return new RazorpayGateway();
-        } else if (gatewayType.equalsIgnoreCase("payu")) {
-            return new PayUGateway();
-        }
-        throw new IllegalArgumentException("Unsupported gateway for India: " + gatewayType);
-    }
 
-    public Invoice createInvoice() {
-        return new GSTInvoice();
-    }
-}
-
-class USFactory implements RegionFactory {
-    public PaymentGateway createPaymentGateway(String gatewayType) {
-        if (gatewayType.equalsIgnoreCase("paypal")) {
-            return new PayPalGateway();
-        } else if (gatewayType.equalsIgnoreCase("stripe")) {
-            return new StripeGateway();
-        }
-        throw new IllegalArgumentException("Unsupported gateway for US: " + gatewayType);
-    }
-
-    public Invoice createInvoice() {
-        return new USInvoice();
-    }
-}
 
 // ========== Checkout Service ==========
 class CheckoutService {
