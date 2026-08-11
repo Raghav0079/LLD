fbd6c46f47427b529081c44187266857e0acf108
Raghav0079 <raghav051006@gmail.com>
2026-08-11 14:33:11 +0530

    commit 46

--- Main17.java
+++ Main17.java
@@ -1,27 +1,55 @@
+import java.util.*;
+
+interface VideoDownloader {
+    String downloadVideo(String videoURL);
+}
+
 // ========== RealVideoDownloader Class ==========
-class RealVideoDownloader {
+class RealVideoDownloader implements VideoDownloader {
+
+    @Override
     public String downloadVideo(String videoUrl) {
-        // caching logic missing
-        // filtering logic missing
-        // access logic missing
         System.out.println("Downloading video from URL: " + videoUrl);
-        String content = "Video content from " + videoUrl;
-        System.out.println("Downloaded Content: " + content);
-        return content;
+        return "Video content from " + videoUrl;
     }
 }
+
+// =============== Proxy With Cache ====================
+class CachedVideoDownloader implements VideoDownloader {
+
+    private RealVideoDownloader realDownloader;
+    private Map<String, String> cache;
+
+    public CachedVideoDownloader() {
+        this.realDownloader = new RealVideoDownloader();
+        this.cache = new HashMap<>();
+    }
+
+    @Override
+    public String downloadVideo(String videoUrl) {
+        if (cache.containsKey(videoUrl)) {
+            System.out.println("Returning cached video for: " + videoUrl);
+            return cache.get(videoUrl);
+        }
+
+        System.out.println("Cache miss. Downloading...");
+        String video = realDownloader.downloadVideo(videoUrl);
+        cache.put(videoUrl, video);
+        return video;
+    }
+}
+
 
 // ================ Main Class ===================
 class Main17 {
     public static void main(String[] args) {
+        VideoDownloader cacheVideoDownloader = new CachedVideoDownloader();
         System.out.println("User 1 tries to download the video.");
-        RealVideoDownloader downloader1 = new RealVideoDownloader();
-        downloader1.downloadVideo("https://video.com/proxy-pattern");
+        cacheVideoDownloader.downloadVideo("https://video.com/proxy-pattern");
 
         System.out.println();
 
         System.out.println("User 2 tries to download the same video again.");
-        RealVideoDownloader downloader2 = new RealVideoDownloader();
-        downloader2.downloadVideo("https://video.com/proxy-pattern");
+        cacheVideoDownloader.downloadVideo("https://video.com/proxy-pattern");
     }
 }
