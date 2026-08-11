97e44c1969379f162310bc0f3b2969b296e763fa
Raghav0079 <raghav051006@gmail.com>
2026-08-11 14:26:59 +0530

    commit 45

---
+++ Main17.java
@@ -0,0 +1,27 @@
+// ========== RealVideoDownloader Class ==========
+class RealVideoDownloader {
+    public String downloadVideo(String videoUrl) {
+        // caching logic missing
+        // filtering logic missing
+        // access logic missing
+        System.out.println("Downloading video from URL: " + videoUrl);
+        String content = "Video content from " + videoUrl;
+        System.out.println("Downloaded Content: " + content);
+        return content;
+    }
+}
+
+// ================ Main Class ===================
+class Main17 {
+    public static void main(String[] args) {
+        System.out.println("User 1 tries to download the video.");
+        RealVideoDownloader downloader1 = new RealVideoDownloader();
+        downloader1.downloadVideo("https://video.com/proxy-pattern");
+
+        System.out.println();
+
+        System.out.println("User 2 tries to download the same video again.");
+        RealVideoDownloader downloader2 = new RealVideoDownloader();
+        downloader2.downloadVideo("https://video.com/proxy-pattern");
+    }
+}
