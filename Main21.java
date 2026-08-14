ba337e2ad4a21bb77f8f44b4ef42e49ce7a53521
Raghav0079 <raghav051006@gmail.com>
2026-08-14 20:12:35 +0530

    commit 60

---
+++ Main21.java
@@ -0,0 +1,19 @@
+
+class YouTubeChannel {
+    public void uploadNewVideo(String videoTitle) {
+        // Upload the video
+        System.out.println("Uploading: " + videoTitle + "\n");
+
+        // Manually notify users
+        System.out.println("Sending email to user1@example.com");
+        System.out.println("Pushing in-app notification to user3@example.com");
+    }
+}
+
+class Main21 {
+    public static void main(String[] args) {
+        // Create a channel and upload a new video
+        YouTubeChannel channel = new YouTubeChannel();
+        channel.uploadNewVideo("Design Patterns in Java");
+    }
+}
