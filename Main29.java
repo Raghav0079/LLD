16c662b6dd8fca90a3869b4225cdb4b33bccc337
Raghav0079 <raghav051006@gmail.com>
2026-09-19 21:38:17 +0530

    commit 83

--- Main29.java
+++ Main29.java
@@ -1,54 +1,70 @@
 import java.util.ArrayList;
 import java.util.List;
 
-// Class representing a User in a collaborative document editor.
-class User {
-    private String name;
-    private List<User> others;  // List of users that have access to this user
+// Mediator Interface
+interface DocumentSessionMediator {
+    void broadcastChange(String change, User sender);
+    void join(User user);
+}
 
-    // Constructor for creating a User with a name.
-    public User(String name) {
-        this.name = name;
-        this.others = new ArrayList<>();
+// Concrete Mediator Class
+class CollaborativeDocument implements DocumentSessionMediator {
+    private List<User> users = new ArrayList<>();
+
+    @Override
+    public void join(User user) {
+        users.add(user);
     }
 
-    // Method to add a collaborator to this user (grants access to the user).
-    public void addCollaborator(User user) {
-        others.add(user);
-    }
-
-    // Method to make a change to the document and notify all collaborators.
-    // Each collaborator will receive the change notification.
-    public void makeChange(String change) {
-        System.out.println(name + " made a change: " + change);
-        for (User u : others) {
-            u.receiveChange(change, this);  // Notify each collaborator about the change.
+    @Override
+    public void broadcastChange(String change, User sender) {
+        for (User user : users) {
+            if (user != sender) {
+                user.receiveChange(change, sender);
+            }
         }
-    }
-
-    // Method to receive a change notification from another user.
-    public void receiveChange(String change, User from) {
-        System.out.println(name + " received: \"" + change + "\" from " + from.name);
     }
 }
 
+// User Class
+class User {
+    protected String name;
+    protected DocumentSessionMediator mediator;
+
+    public User(String name, DocumentSessionMediator mediator) {
+        this.name = name;
+        this.mediator = mediator;
+    }
+
+    // Method for users to make a change
+    public void makeChange(String change) {
+        System.out.println(name + " edited the document: " + change);
+        mediator.broadcastChange(change, this);
+    }
+
+    // Method to receive a change from another user
+    public void receiveChange(String change, User sender) {
+        System.out.println(name + " saw change from " + sender.name + ": \"" + change + "\"");
+    }
+}
 
 // Client Code
 class Main29 {
     public static void main(String[] args) {
+        CollaborativeDocument doc = new CollaborativeDocument();
+
         // Creating users
-        User alice = new User("Alice");
-        User bob = new User("Bob");
-        User charlie = new User("Charlie");
+        User alice = new User("Alice", doc);
+        User bob = new User("Bob", doc);
+        User charlie = new User("Charlie", doc);
 
-        // Adding collaborators (Alice gives access to Bob and Charlie)
-        alice.addCollaborator(bob);
-        alice.addCollaborator(charlie);
+        // Joining the collaborative document
+        doc.join(alice);
+        doc.join(bob);
+        doc.join(charlie);
 
-        // Alice makes a change, notifying Bob and Charlie
-        alice.makeChange("Updated the document title");
-
-        // Bob makes a change, but no collaborators are notified because Bob has no collaborators added
-        bob.makeChange("Added a new section to the document");
+        // Users making changes
+        alice.makeChange("Added project title");
+        bob.makeChange("Corrected grammar in paragraph 2");
     }
 }
